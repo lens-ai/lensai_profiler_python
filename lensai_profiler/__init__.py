edaf79d19435717c5c2955f46d110d56b72a88b1
Venkata Pydipalli <vsnm.tej@gmail.com>
2024-08-21 14:27:06 +0200

    Added the method to init script in the library.

--- lensai_profiler/__init__.py
+++ lensai_profiler/__init__.py
@@ -2,37 +2,19 @@
 
 import sys
 
-# Define the functions to be conditionally imported based on available frameworks
-__all__ = [
-    'calculate_brightness',
-    'calculate_snr',
-    'calculate_sharpness_laplacian',
-    'calculate_channel_mean',
-    'process_batch',
-    'get_histogram_sketch',
-    'calculate_percentiles',
-    'Sketches'
-]
+# lensai_profiler/__init__.py
 
-try:
-    from .metrics import (
-        calculate_brightness,
-        calculate_snr,
-        calculate_sharpness_laplacian,
-        calculate_channel_mean,
-        process_batch,
-        get_histogram_sketch,
-        calculate_percentiles
-    )
-except ImportError as e:
-    if 'tensorflow' in str(e) or 'torch' in str(e):
-        sys.stderr.write(
-            "Error: TensorFlow or PyTorch is required to use some metrics functions. "
-            "Please install the appropriate package with 'pip install your_package_name[tensorflow]' "
-            "or 'pip install your_package_name[torch]'.\n"
-        )
-        sys.exit(1)
-    else:
-        raise
+from .metrics import (
+    Metrics,
+    calculate_percentiles,
+    get_histogram_sketch
+)
 
 from .sketches import Sketches
+
+__all__ = [
+    'Metrics',
+    'calculate_percentiles',
+    'get_histogram_sketch',
+    'Sketches'
+]
