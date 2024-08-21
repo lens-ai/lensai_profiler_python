a1a8ea04ed60e942adc3fce3395b7d087960af96
Venkata Pydipalli <vsnm.tej@gmail.com>
2024-08-21 12:55:13 +0200

    Removed the check of tf and pytorch from setup.py.

--- setup.py
+++ setup.py
@@ -1,30 +1,6 @@
 from setuptools import setup, find_packages
 import sys
 
-# Check if TensorFlow or PyTorch is installed
-tf_installed = False
-torch_installed = False
-
-try:
-    import tensorflow as tf
-    tf_installed = True
-except ImportError:
-    pass
-
-try:
-    import torch
-    torch_installed = True
-except ImportError:
-    pass
-
-if not tf_installed and not torch_installed:
-    sys.stderr.write(
-        "Error: Either TensorFlow or PyTorch must be installed to use this library.\n"
-        "Please install the package with 'pip install your_package_name[tensorflow]' "
-        "or 'pip install your_package_name[torch]'.\n"
-    )
-    sys.exit(1)
-
 setup(
     name='lensai_profiler',
     version='1.1.0',
