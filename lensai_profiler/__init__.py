d793995e1e6646794f2a9ed068698e5a0d2188e9
Venkata Pydipalli <vsnm.tej@gmail.com>
2024-08-21 14:03:42 +0200

    Added the method to init script in the library.

--- lensai_profiler/__init__.py
+++ lensai_profiler/__init__.py
@@ -1,13 +1,38 @@
-# lensai/__init__.py
+# lensai_profiler/__init__.py
 
-from .metrics import (
-    calculate_brightness,
-    calculate_snr,
-    calculate_sharpness_laplacian,
-    calculate_channel_mean,
-    process_batch,
-    get_histogram_sketch,
-    calculate_percentiles
-)
+import sys
+
+# Define the functions to be conditionally imported based on available frameworks
+__all__ = [
+    'calculate_brightness',
+    'calculate_snr',
+    'calculate_sharpness_laplacian',
+    'calculate_channel_mean',
+    'process_batch',
+    'get_histogram_sketch',
+    'calculate_percentiles',
+    'Sketches'
+]
+
+try:
+    from .metrics import (
+        calculate_brightness,
+        calculate_snr,
+        calculate_sharpness_laplacian,
+        calculate_channel_mean,
+        process_batch,
+        get_histogram_sketch,
+        calculate_percentiles
+    )
+except ImportError as e:
+    if 'tensorflow' in str(e) or 'torch' in str(e):
+        sys.stderr.write(
+            "Error: TensorFlow or PyTorch is required to use some metrics functions. "
+            "Please install the appropriate package with 'pip install your_package_name[tensorflow]' "
+            "or 'pip install your_package_name[torch]'.\n"
+        )
+        sys.exit(1)
+    else:
+        raise
 
 from .sketches import Sketches
